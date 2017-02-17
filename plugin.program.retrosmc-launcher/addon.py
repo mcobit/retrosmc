b560f229072d180346eae8e1e6b4ffee418da95f
mcobit <mcobit@gmx.de>
2017-02-17 22:33:34 +0100

    Fixes for the launcher

--- plugin.program.retrosmc-launcher/addon.py
+++ plugin.program.retrosmc-launcher/addon.py
@@ -27,8 +27,10 @@ if os.path.exists( "/home/osmc/install-retrosmc.sh" ):
 else:
   if dialog.yesno("Warning","RetrOSMC is not installed. Should we start the installation?","Be sure to have a working internet connection and a joypad and/or keyboard connected!",""):
     os.popen("cd && wget https://raw.githubusercontent.com/mcobit/retrosmc/testing/install-retrosmc.sh")
+    os.popen("cd && mkdir -p /home/osmc/RetroPie/scripts && wget -o /home/osmc/RetroPie/scripts/start-install.sh https://raw.githubusercontent.com/mcobit/retrosmc/testing/scripts/start-install.sh")
+    os.popen("cd && mkdir -p /home/osmc/RetroPie/bin && wget -o /home/osmc/RetroPie/bin/restrosmc_helper https://raw.githubusercontent.com/mcobit/retrosmc/testing/bin/retrosmc_helper")
     if os.path.exists( "/home/osmc/install-retrosmc.sh" ):
-      os.popen("cd && chmod +x /home/osmc/install-retrosmc.sh")
+      os.popen("cd && chmod +x /home/osmc/install-retrosmc.sh && chmod +x /home/osmc/RetroPie/scripts/* && chmod +x /home/osmc/RetroPie/bin/*")
       os.popen("/home/osmc/RetroPie/scripts/start-install.sh")
     else:
       dialog.ok("Error","Could not download the installerscript.\n\nCheck your internetconnection and try again!")
