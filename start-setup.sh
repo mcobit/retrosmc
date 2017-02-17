05025504478e0fbe61218bf1a2b264b6992b2672
mcobit <mcobit@gmx.de>
2017-02-17 21:32:45 +0100

    Delete start-setup.sh

--- start-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/sbin:/usr/sbin:/usr/osmc/bin:/opt/vc/bin:/usr/local/sbin
-
-  cd
-  sudo su osmc -c "/home/osmc/test setup &" & disown
-  sleep 0.5
-  sudo su -c "systemctl stop mediacenter &" &
-
-exit
