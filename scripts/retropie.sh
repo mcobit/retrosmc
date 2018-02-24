8eefde7071f57dfd7a2ab3c0b9c355f7d387e224
Pavel V. Dimens <pdimens@live.com>
2018-02-24 12:10:56 -0600

    replace with pegasus-fe

--- scripts/retropie.sh
+++ scripts/retropie.sh
@@ -15,31 +15,31 @@ sudo su osmc -c "sh /home/osmc/RetroPie/scripts/retropie_watchdog.sh &" &
 
 # check if emulationstation script in chroot is changed and if so, create altered script
 
-sudo chown osmc:osmc /usr/bin/emulationstation
+sudo chown osmc:osmc /usr/bin/pegasus-fe
 
 echo '#!/bin/bash
-es_bin="/opt/retropie/supplementary/emulationstation/emulationstation"
+es_bin="/opt/retropie/supplementary/pegasus-fe/pegasus-fe"
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/sbin:/usr/sbin:/usr/osmc/bin:/opt/vc/bin
 
 if [[ $(id -u) -eq 0 ]]; then
-    echo "emulationstation should not be run as root. If you used 'sudo emulationstation' please run without sudo."
+    echo "pegasus-fe should not be run as root. If you used 'sudo pegasus-fe' please run without sudo."
     exit 1
 fi
 if [[ -n "$(pidof X)" ]]; then
     echo "X is running. Please shut down X in order to mitigate problems with loosing keyboard input. For example, logout from LXDE."
     exit 1
 fi
-$es_bin "$@"' > "/usr/bin/emulationstation"
+$es_bin "$@"' > "/usr/bin/pegasus-fe"
 
-# start emulationstation on vitrual terminal 7 and detach it
+# start pegasus-fe on vitrual terminal 7 and detach it
 
-sudo su osmc -c "nohup openvt -c 7 -f -s emulationstation >/dev/null 2>&1 &" &
+sudo su osmc -c "nohup openvt -c 7 -f -s pegasus-fe >/dev/null 2>&1 &" &
 
 # clear the screen again
 
 sudo openvt -c 7 -s -f clear
 
-# wait a bit to make sure emulationstation is running detached from kodi
+# wait a bit to make sure pegasus-fe is running detached from kodi
 
 sleep 0.5
 
